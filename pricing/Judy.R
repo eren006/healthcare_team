029725263952fcc05464dacde0ea89cdc6068ac0
judyC29 <judy.fu@rotman.utoronto.ca>
2025-03-06 23:32:17 -0500

    Judy - update

---
+++ pricing/Judy.R
@@ -0,0 +1,20 @@
+# load in the data file into data.frame (you can use import as well)
+library(readxl)
+library(dplyr)
+library(ggplot2)
+library(caret)
+library(stats)
+library(dplyr)
+library(tidyr)
+
+# load in the data file into data.frame
+data <- read_excel("/Users/judyfu/Desktop/Shared Github/8416/pricing/data.xlsx", sheet = "MH-Modified Data")
+
+# Inspect the data (additional)
+str(data)
+
+#take a look at the summary statistics
+summary(data)
+
+head(data)
+
