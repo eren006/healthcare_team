231ad19706bed6026bc5c8b1f2e68e57a6145a9d
zyaooooo <lizhouyao1@gmail.com>
2025-03-11 14:15:00 -0400

    create dummy variable

--- pricing/Yolanda.R
+++ pricing/Yolanda.R
@@ -1,12 +1,12 @@
 rm(list=ls())
 
-df <- read_excel("C:/Users/25766/Desktop/healthcare_team/pricing/data_cleaned.xlsx", sheet = 1)
+library(readxl)
+df <- read_excel("pricing/data.xlsx", sheet = "MH-Modified Data")
 
-# **去除列名中的所有空格**
-colnames(df) <- gsub(" ", "", colnames(df))  
-print(colnames(df))
+df <- df %>% clean_names()
 
-df <- df %>%  # 确保更新 df
+#create dummy variable
+df <- df %>% 
   mutate(is_Female = ifelse(trimws(gender) == "F", 1, 0)) %>%  
   select(-gender) 
 
@@ -15,52 +15,21 @@ df <- df %>%
   select(-marital_status)  
 
 df <- df %>%
-  mutate(is_ACHD = ifelse(key_complaints_code == "ACHD", 1, 0),
-         is_CAD_DVD = ifelse(key_complaints_code == "CAD-DVD", 1, 0),
-         is_CAD_TVD = ifelse(key_complaints_code == "CAD-TVD", 1, 0),
-         is_CAD_VSD = ifelse(key_complaints_code == "CAD-VSD", 1, 0),
-         is_OS_ASD = ifelse(key_complaints_code == "OS-ASD", 1, 0),
-         is_other_heart = ifelse(key_complaints_code == "other- heart", 1, 0),
-         is_other_respiratory = ifelse(key_complaints_code == "other- respiratory", 1, 0),
-         is_other_general = ifelse(key_complaints_code == "other-general", 1, 0),
-         is_other_nervous = ifelse(key_complaints_code == "other-nervous", 1, 0),
-         is_other_tertalogy = ifelse(key_complaints_code == "other-tertalogy", 1, 0),
-         is_PM_VSD = ifelse(key_complaints_code == "PM-VSD", 1, 0),
-         is_RHD = ifelse(key_complaints_code == "RHD", 1, 0)) %>%
   select(-key_complaints_code)  
 
 df <- df %>%
-  mutate(is_Diabetes1 = ifelse(past_medical_history_code == "Diabetes1", 1, 0),
-         is_Diabetes2 = ifelse(past_medical_history_code == "Diabetes2", 1, 0),
-         is_Hypertension1 = ifelse(past_medical_history_code == "hypertension1", 1, 0),
-         is_Hypertension2 = ifelse(past_medical_history_code == "hypertension2", 1, 0),
-         is_Hypertension3 = ifelse(past_medical_history_code == "hypertension3", 1, 0),
-         is_Other = ifelse(past_medical_history_code == "other", 1, 0)) %>% 
-  select(-past_medical_history_code)  # 删除原始变量
+  select(-past_medical_history_code)  
 
 df <- df %>%
-  mutate(is_Ambulance = ifelse(mode_of_arrival == "AMBULANCE", 1, 0),
-         is_Transferred = ifelse(mode_of_arrival == "TRANSFERRED", 1, 0)) %>% 
-  select(-mode_of_arrival)  # 删除原始变量
+  select(-mode_of_arrival)  
 
-#delete "state_at_the_time_of_arrival" 
 df <- df %>% select(-state_at_the_time_of_arrival)  
 
 df <- df %>%
-  mutate(is_Emergency = ifelse(trimws(type_of_admsn) == "EMERGENCY", 1, 0)) %>%
   select(-type_of_admsn) 
 
 df <- df %>%
-  mutate(is_implant = ifelse(trimws(implant_used_y_n) == "Y", 1, 0)) %>%
   select(-implant_used_y_n)
-
-
-#delete column with only 1 variable
-#delete "alert" 
-df <- df %>% select(-alert)  
-#delete "alert"
-df <- df %>% select(-cad_svd) 
-
 
 #question f
 y <- df$total_cost_to_hospital
@@ -71,3 +40,5 @@ df_scaled <- cbind(y, X)
 #full model
 full_model <- lm(y ~ ., data = df_scaled)
 summary(full_model)
+
+
