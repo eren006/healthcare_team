f88d0399e3d8540aad5a6b0330712d1bf6bff133
zyaooooo <lizhouyao1@gmail.com>
2025-04-22 15:38:00 -0400

    question f and g

--- policy/Yolanda.R
+++ policy/Yolanda.R
@@ -6,7 +6,7 @@ library(readxl)
 library(dplyr)
 library(fixest)  
 
-df <- read_excel("C:/Users/25766/Desktop/多伦多/health care/assignment 4/overtime_data-4.xlsx")
+df <- read_excel("policy/overtime_data-4.xlsx")
 
 # missing value
 colSums(is.na(df))
@@ -53,4 +53,34 @@ model <- feols(
 
 summary(model)
 
+#question g
+
+library(dplyr)
+library(ggplot2)
+
+#treated and controlled states
+treated_states <- c("AK", "NH", "NY", "PA")
+
+df <- df %>%
+  mutate(group = ifelse(state %in% treated_states, "Treated", "Control"))
+
+# calculate mean curhdef 
+avg_def <- df %>%
+  group_by(group, year) %>%
+  summarise(mean_def = mean(curhdef, na.rm = TRUE), .groups = "drop")
+
+avg_treatment_year <- 2009
+
+ggplot(avg_def, aes(x = year, y = mean_def, color = group)) +
+  geom_line(size = 1.2) +
+  geom_point(size = 2) +
+  geom_vline(xintercept = avg_treatment_year, linetype = "dashed", color = "gray") +
+  labs(
+    title = "Mean Deficiency Citations: Treated vs. Control States",
+    x = "Year",
+    y = "Mean Deficiency Citations (curhdef)",
+    color = "Group"
+  ) +
+  scale_color_manual(values = c("Treated" = "red", "Control" = "black")) +
+  theme_minimal()
 
